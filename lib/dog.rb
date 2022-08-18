b0fc84c6408c94f2ac677e39b234783904282fae
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-08-18 20:13:43 +0300

    Completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
 
+    def name=(name)
+        @name=name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed=breed
+    end
+
+    def breed
+        @breed
+    end
+
 end
+
+doggy = Dog.new
+
+puts doggy.name= "German Shephard"
+
+another_doggy = Dog.new
+puts doggy.name = "Terrie"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
+    def name=(name)
+        @name=name
+    end
 
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
 end
+
+name_me = Person.new
+puts name_me.name = "Kamau"
+
+puts name_me.name = "Martin Kamau"
+
